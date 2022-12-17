a4521a86b8a3beaaa7d8b0fdbbf18dec44173d52
Denis Costello <G00396309@atu.ie>
2022-12-17 10:50:14 +0000

    First commit

---
+++ src/main/java/ie/atu/Passenger2/Passenger2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.Passenger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Passenger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Passenger2Application.class, args);
+	}
+
+}
